23e8241dbc0a2b095b1ab48804d1af7d9f11d458
정우git config --global user.name <wjddntjd119@email.daelim.ac.kr>
2022-11-17 12:14:37 +0900

    초기 내용

---
+++ 13/js/change-color.js
@@ -0,0 +1,10 @@
+const heading = document.querySelector('#heading');
+
+heading.onclick = function() {
+  heading.style.color = 'red';
+};
+
+// eslint-disable-next-line no-unused-vars
+const data = 'text';
+
+console.log(data);
