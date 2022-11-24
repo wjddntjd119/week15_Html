1d1c3ecc8638b6b39f7ed432cad8426f09f8b8de
정우git config --global user.name <wjddntjd119@email.daelim.ac.kr>
2022-11-24 16:26:09 +0900

    week14

--- 13/js/change-color.js
+++ 13/js/change-color.js
@@ -5,6 +5,11 @@ heading.onclick = function() {
 };
 
 // eslint-disable-next-line no-unused-vars
+/*
 const data = 'text';
 
 console.log(data);
+*/
+
+var name = '홍길동';
+		console.log("이름은 ${name}");
