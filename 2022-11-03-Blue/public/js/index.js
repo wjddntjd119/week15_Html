f3ca4da1b22cf42b60f19a4abd052fd207d677cd
정우git config --global user.name <wjddntjd119@email.daelim.ac.kr>
2022-12-08 12:00:41 +0900

    주석추가

--- 2022-11-03-Blue/public/js/index.js
+++ 2022-11-03-Blue/public/js/index.js
@@ -1,20 +1,23 @@
+//클래스 이름 user인 값 호출
 const headeruserNameElement = document.querySelector('.user');
 
+//userName 이라고 선언된 값을 localuserName에 넣음
 const localuserName = localStorage.getItem('userName');
+
+//아이디 이름이 name인 값 호출
 const userNameElement = document.querySelector('#name');
 
+//입력받은 값으로 바꿔주는 setUserNameInnerHtml 함수 선언.
 const setUserNameInnerHtml = (name) => {
   headeruserNameElement.innerHTML = `${name}<span>님</span>`;
   userNameElement.textContent = name;
 };
-
+//null 값이면 원래 값 입력.
 if (localuserName) {
   setUserNameInnerHtml(localuserName);
 }
 
-// eslint-disable-next-line max-len
-// userNameElement.innerHTML = `${localStorage.getItem('userName')}<span>님</span>`;
-
+//userNameElement를 누르면 이름입력창이 뜨고 그 이름을 userName값에 저장
 userNameElement.onclick = () => {
   const userName = prompt('이름을 입력해 주세요');
   localStorage.setItem('userName', userName); // 새로고침을 해도 값이 유지됨
