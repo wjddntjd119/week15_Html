242afe8945e9840c68910672bb7de6a6d26988a3
정우git config --global user.name <wjddntjd119@email.daelim.ac.kr>
2022-12-08 11:40:40 +0900

    사용자 이름 입력 받는 기능 추가

--- 2022-11-03-Blue/public/js/index.js
+++ 2022-11-03-Blue/public/js/index.js
@@ -1,2 +1,25 @@
-const test = 10;
-console.log(test);
+//다른 파일에서 특정 클래스명 불러오기
+
+const headerUserNameElement = document.querySelector('.user');
+
+const userNameElement = document.querySelector('.user-name');
+
+const localUserName = localStorage.getItem('userName');
+
+const setUserNameInnerHTML = (name) => {
+  headerUserNameElement.innerHTML = `${name} <span>님</span>`;
+  userNameElement.innerHTML = `${name} <span>님</span>`;
+};
+
+if (localUserName) {
+  setUserNameInnerHTML(localUserName);
+}
+
+userNameElement.onclick = () =>{
+  const userName = prompt('이름을 입력해주세요');
+  //username에 대한 유효성 검사 잇으면 좋다
+
+  localStorage.setItem('userName', userName);
+
+  setUserNameInnerHTML(userName);
+};
